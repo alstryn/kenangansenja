cc2e6dd4c2857e48616b9782d9d7e544715018fd
alstryn <auliyamaharlika44@gmail.com>
2025-06-24 08:57:32 +0700

    Search

--- css/js/script.js
+++ css/js/script.js
@@ -1,4 +1,4 @@
-// Toggle Class ActiveAdd commentMore actions
+// Toggle Class Active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika Hamburger Menu Di Klik
@@ -9,8 +9,25 @@ document.querySelector("#hamburger-menu").onclick = () => {
 // klik diluar sidebar untuk menghilangkan sidebar
 const hamburger = document.querySelector("#hamburger-menu");
 
+// Toggle class active untuk search fromAdd commentMore actions
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
